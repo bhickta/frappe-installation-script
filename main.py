12112652177becfb2147af71e7682bd326663f63
bhickta <nishantbhickta@gmail.com>
2023-12-20 23:32:18 +0530

    removed env and decouple

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from server.setup import Setup
+from config.config import Config
+
+
 def main():
-    Setup().setup()
+    Setup(Config).setup()
     # mysql_secure_installation()
     # mysql_conf()
 
     # # frappe-init
     # frappe_install()
+
 
 if __name__ == "__main__":
     main()
